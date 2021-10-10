090919845e761b201846b867345f4ff5abd0d828
Pragati Chothe <pragatichothe@gmail.com>
2021-10-10 14:00:45 +0530

    seperate check_for_win function from app.js

---
+++ check_if_won.js
@@ -0,0 +1,14 @@
+function checkForWin() {
+    ///simplified win argument
+  let matches = 0
+
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i].classList.contains('flag') && squares[i].classList.contains('bomb')) {
+        matches ++
+      }
+      if (matches === bombAmount) {
+        result.innerHTML = 'YOU WIN!'
+        isGameOver = true
+      }
+    }
+}

365ad824f89e8f579449fcaabd6a0885bccac52d
Pragati Chothe <pragatichothe@gmail.com>
2022-01-06 14:15:25 +0530

    add var in app.js start of time interval

--- js/app.js
+++ js/app.js
@@ -8,11 +8,17 @@ var grid;
 var flagsLeft;
 var result;
 var interval;
+var timeInterval;
+var timeSeconds=0;
+var minutes;
+var seconds;
 
 document.addEventListener("DOMContentLoaded", () => {
   grid = document.querySelector(".gri1");
   flagsLeft = document.querySelector("#flags-left");
   result = document.querySelector("#result");
-
+  minutes = document.querySelector("#minutes");
+  seconds = document.querySelector("#seconds");
+  
   createBoard();
 })

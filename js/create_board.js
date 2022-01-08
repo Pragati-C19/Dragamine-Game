5502ffe23947ff2c875b6b46a0bf29c12d973d3e
Pragati Chothe <pragatichothe@gmail.com>
2022-01-08 21:35:02 +0530

    remove FIXME comment

--- js/create_board.js
+++ js/create_board.js
@@ -72,29 +72,5 @@ function createBoard() {
   }
 
   timerCycle();
-  /* //FIXME: if timer system not working then remove this comment
-  const getTwoDigitStr = (num) => {
-    //For adding 0 befor one digit number
-    if (num <= 9) return `0${num}`;
-    return num;
-  };
-
-  timeInterval = setInterval(() => {
-    timerSeconds++;
-    seconds = timerSeconds % 60;
-    minutes = Math.floor(timerSeconds / 60);
-    //console.log(seconds)
-    sec.innerHTML = getTwoDigitStr(seconds);
-    min.innerHTML = getTwoDigitStr(minutes);
-
-    // you have to add clearInterval here not in game_over.js if we use clearInterval in game_over.js or in check_if_won.js it gives infinite timercount
-    if (isGameOver == true) {
-      clearInterval(timerInterval);
-      //console.log(timerSeconds);
-      timerSeconds = 0;
-      sec.innerHTML = getTwoDigitStr(timerSeconds);
-      min.innerHTML = getTwoDigitStr(timerSeconds);
-    }
-  }, 1000); */
 }
 createBoard(); // call function
